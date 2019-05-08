a64b7ed6f5471ff4b3b35406f7317b88c9797e88
Unknown <yuliya.sm7@gmail.com>
2019-05-09 01:26:43 +0300

    test RS485

--- VIM/VIM.ino
+++ VIM/VIM.ino
@@ -1,9 +1,9 @@
 #include <EEPROMex.h>
 
-const int param_count = 4;
-char* param[param_count] = {"DIAMETR", "GAP", "PLOT", "CELLS"};
+const int param_count = 5;
+char* param[param_count] = {"CELLS", "GAP", "PLOT", "DIAMETR", "MICROSTEP"};  // диаметр колеса, промежуток, делянка, количество ячеек в касете
 int value[param_count];
-const int addr[param_count] = {0, 2, 4, 6};
+const int addr[param_count] = {0, 2, 4, 6, 8};
 
 const uint8_t  pinDebug  = 13;
 const uint8_t  pinTable  = 0;
@@ -11,36 +11,33 @@ const uint8_t  pinDispenser  = 0;
 const uint8_t  pinEncoder  = 21;
 
 const int BUFF  = 128;
-const int MICROSTEP  = 16;
 
-int  ENCODER_STEP  = 20;  // см
-int cell_count = 0;
-long long L = 0;  // пройденное расстояние, см
-long gap = 0;
+int  ENCODER_STEP  = 5;  // шаг одного импульса энкодера, см
+int cells_count = 0;  // счетчик оставшихся ячеек
+long long L = 0;  // суммарное пройденное расстояние, см
+long distance = 0;  // расстояние от предыдущего высева, см
 bool fl_run = false;
 
 void setup() {
   attachInterrupt(2, interruptHall, RISING);  // pin 21 по фронту
   Serial2.begin(9600);  // display
-  Serial3.begin(9600);  // RS-485
+  Serial3.begin(9600, SERIAL_8E1);  // RS-485
   Serial.begin(9600);  // debug
   pinMode(pinDebug, OUTPUT);
   pinMode(pinDispenser, OUTPUT);
-  
-  value[0] = EEPROM.readInt(addr[0]);  // диаметр колеса
-  value[1] = EEPROM.readInt(addr[1]);  // промежуток
-  value[2] = EEPROM.readInt(addr[2]);  // делянка
-  value[3] = EEPROM.readInt(addr[3]);  // количество ячеек в касете
 
+  for (int i = 0; i < param_count; ++i) {
+    value[i] = EEPROM.readInt(addr[i]);
+  }
   Serial.println("Init");
 }
 
 void loop() {
-  if (gap >= value[1]) {
+  if (distance >= value[1] && fl_run) {
     digitalWrite(pinDispenser, HIGH);
     delay(2000);
     digitalWrite(pinDispenser, LOW);
-    gap = 0;
+    distance = 0;
   }
 }
 
@@ -71,56 +68,65 @@ void serialEvent2() {  // display
   char message[BUFF];
   receive(Serial2, message);
   if (!fl_run) {
-    if (strstr(message, "RUN")) {
+    if (strstr(message, "ON")) {
       fl_run = true;
-    }else if (!setConfig(message)) {
+    } else if (!setConfig(message)) {
       DriverManual(message);
     }
+  } else if (strstr(message, "OFF")) {
+    fl_run = false;
+  } else if (strstr(message, "request")) {
+    // ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ??
+  } else {
+    Serial.println("Invalid command");
   }
 }
 
 void serialEvent3() {  // rs485
   char message[BUFF];
   receive(Serial3, message);
+  Serial.println(message);
+  Serial.write(message);
   if (strstr(message, "E16")) {
-    Serial.println("Error");
+    Serial.println("Stepper Motor Error");
   }
 }
 
 bool setConfig(char * message) {
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < param_count; ++i) {
     if (strstr(message, param[i])) {
       value[i] = readNum(strstr(message, param[i]), strlen(param[i]));
       EEPROM.updateInt(addr[i], value[i]);
-      Serial.print("write ");
-      Serial.println(param[i]);
-      Serial.println(value[i]);
+      Serial.print("write "); Serial.print(param[i]); Serial.println(value[i]);
       return true;
     }
   }
-  return true;
+  return false;
 }
 
 void DriverManual(char * message) {
-  if (strstr(message, "FORVARD")) {
-    int sp = readNum(strstr(message, "FORVARD"), strlen("FORVARD"));
-    Serial3.write(":01SD");
-    Serial3.write(sp);
-    Serial3.write("\r\n");
-    Serial3.write(":01MV\r\n");
-  } else if (strstr(message, "STOP")) {
-    Serial3.write(":01 \r\n");
+  if (strstr(message, "STOP")) {
+    Serial3.print(":01MV1\r\n"); Serial.println("stop");
+    return;
   }
+  int sp = 0;
+  if (strstr(message, "RIGHT")) {
+    sp = readNum(strstr(message, "RIGHT"), strlen("RIGHT"));
+    Serial3.print(":01DR\r\n"); Serial.println("moveR");
+  } else if (strstr(message, "LEFT")) {
+    sp = readNum(strstr(message, "LEFT"), strlen("LEFT"));
+    Serial3.print(":01DL\r\n"); Serial.println("moveL");
+  } else return;
+  delay(200);
+  Serial.println(sp);
+  Serial3.print(":01SD"); Serial3.print(sp); Serial3.print("\r\n");
+  delay(200);
+  Serial3.print(":01MV\r\n");
 }
 
 void interruptHall() {
   if (fl_run) {
     L += ENCODER_STEP;
-    gap += ENCODER_STEP;
+    distance += ENCODER_STEP;
   }
 }
-/*void DriverInit() {
-  Serial3.write(":01AL200\r\n"); // ускорение
-  Serial3.write(":01SD300\r\n"); // скорость
-  Serial3.write(":01MV100\r\n"); // 100 шагов
-}*/
